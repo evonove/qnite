fc90acd37e9f0173e28ab7232a84bc656202a4b6
Massimiliano Pippi <mpippi@gmail.com>
2015-04-21 12:46:07 +0200

    first draft of Ticker base type

--- modules/Qnite/plugin/qniteticker.cpp
+++ modules/Qnite/plugin/qniteticker.cpp
@@ -1,5 +1,3 @@
-#include "qniteticker.h"
-
 /*!
     \qmltype Ticker
     \instantiates QniteTicker
@@ -9,9 +7,83 @@
 
     TODO: add a long description
 */
+#include "qniteticker.h"
+
+
+// utility functions
+namespace {
+
+    template<typename T>
+    QList<T> fromVariantList(const QVariantList& in)
+    {
+        QList<T> out;
+        foreach(QVariant val, in) {
+            out.append(val.value<T>());
+        }
+        return out;
+    }
+
+    template<typename T>
+    QVariantList toVariantList(const QList<T>& in)
+    {
+        QVariantList out;
+        foreach(T val, in) {
+            out.append(QVariant(val));
+        }
+        return out;
+    }
+
+}
 
 QniteTicker::QniteTicker(QObject * parent)
     : QObject(parent)
 {
 
+}
+
+QVariantList QniteTicker::values() const
+{
+    return toVariantList<float>(values_);
+}
+
+void QniteTicker::setValues(QVariantList& values)
+{
+    values_ = fromVariantList<float>(values);
+
+    // compute min and max bounds
+    qSort(values_.begin(), values_.end(), std::greater<float>());
+    lowerBound_ = values_.first();
+    upperBound_ = values_.last();
+
+    emit valuesChanged();
+}
+
+void QniteTicker::setMinorTicks(QVariantList& ticks)
+{
+    minorTicks_ = fromVariantList<float>(ticks);
+}
+
+void QniteTicker::setMidTicks(QVariantList& ticks)
+{
+    midTicks_ = fromVariantList<float>(ticks);
+}
+
+void QniteTicker::setMajorTicks(QVariantList& ticks)
+{
+    majorTicks_ = fromVariantList<float>(ticks);
+}
+
+QVariantList QniteTicker::minorTicks() const
+{
+    return toVariantList<float>(minorTicks_);
+}
+
+QVariantList QniteTicker::midTicks() const
+{
+    return toVariantList<float>(midTicks_);
+}
+
+QVariantList QniteTicker::majorTicks() const
+{
+    return toVariantList<float>(majorTicks_);
 }
--- modules/Qnite/plugin/qniteticker.h
+++ modules/Qnite/plugin/qniteticker.h
@@ -2,21 +2,50 @@
 #define QNITETICKER_H
 
 #include <QObject>
+#include <QVariant>
+
 
 class QniteTicker : public QObject
 {
     Q_OBJECT
     Q_DISABLE_COPY(QniteTicker)
 
-    Q_PROPERTY(QVariantList xValues READ xValues WRITE setXValues NOTIFY xValuesChanged)
+    Q_PROPERTY(QVariantList values READ values WRITE setValues NOTIFY valuesChanged)
+    Q_PROPERTY(QVariantList minorTicks READ minorTicks WRITE setMinorTicks)
+    Q_PROPERTY(QVariantList midTicks READ midTicks WRITE setMidTicks)
+    Q_PROPERTY(QVariantList majorTicks READ majorTicks WRITE setMajorTicks)
+
+signals:
+    void valuesChanged();
 
 public:
     explicit QniteTicker(QObject * parent=0);
     virtual ~QniteTicker() {}
 
+    void setValues(QVariantList&);
+    void setBoundaries(float lower, float upper);
+    void setMinorTicks(QVariantList&);
+    void setMidTicks(QVariantList&);
+    void setMajorTicks(QVariantList&);
+
+    QVariantList values() const;
+    QVariantList minorTicks() const;
+    QVariantList midTicks() const;
+    QVariantList majorTicks() const;
+
+    float lower() const { return lowerBound_; }
+    float upper() const { return upperBound_; }
+
 protected:
     virtual void buildTicks() = 0;
 
+    QList<float> minorTicks_;
+    QList<float> midTicks_;
+    QList<float> majorTicks_;
+    QList<float> values_;
+
+    float lowerBound_;
+    float upperBound_;
 };
 
 #endif // QNITETICKER_H
