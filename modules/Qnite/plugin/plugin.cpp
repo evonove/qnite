d87cd6e2da63ce3e7a482c93ff06b7d436556958
Massimiliano Pippi <mpippi@gmail.com>
2015-04-21 12:45:47 +0200

    register Ticker and LinearTicker qml types

--- modules/Qnite/plugin/plugin.cpp
+++ modules/Qnite/plugin/plugin.cpp
@@ -2,6 +2,8 @@
 
 #include "qnitecurve.h"
 #include "qnitescalemap.h"
+#include "qniteticker.h"
+#include "qnitelinearticker.h"
 
 #include <QtQml>
 
@@ -10,5 +12,8 @@ void QnitePlugin::registerTypes(const char *uri)
   // @uri qnite
   qmlRegisterType<QniteCurve>(uri, 1, 0, "Curve");
   qmlRegisterType<QniteScaleMap>(uri, 1, 0, "ScaleMap");
+  qmlRegisterUncreatableType<QniteTicker>(uri, 1, 0, "Ticker",
+    "Cannot create objects of base type Ticker");
+  qmlRegisterType<QniteLinearTicker>(uri, 1, 0, "LinearTicker");
 }
 
