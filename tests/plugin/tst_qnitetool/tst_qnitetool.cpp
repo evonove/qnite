3f16e3c384804e8138ab9f184c1fe66aa5ab4ce1
Federico Frenguelli <synasius@gmail.com>
2015-05-14 15:02:55 +0200

    added an instance test

--- tests/plugin/tst_qnitetool/tst_qnitetool.cpp
+++ tests/plugin/tst_qnitetool/tst_qnitetool.cpp
@@ -2,6 +2,14 @@
 
 #include "tools/qnitetool.h"
 
+class FooTool: public QniteTool
+{
+  Q_OBJECT
+public:
+  FooTool(QQuickItem* parent = 0):
+    QniteTool(parent)
+  {}
+};
 
 class TestQniteTool: public QObject
 {
@@ -9,6 +17,14 @@ class TestQniteTool: public QObject
 
 private slots:
 
+  void testInstance();
+
 };
+
+void TestQniteTool::testInstance()
+{
+  FooTool f;
+}
+
 QTEST_MAIN(TestQniteTool)
 #include "tst_qnitetool.moc"
