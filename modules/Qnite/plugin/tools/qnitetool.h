fb8e52944050f1030fee72290494f4f8604941f9
Massimiliano Pippi <mpippi@gmail.com>
2015-05-22 11:37:42 +0200

    added tool testing

--- modules/Qnite/plugin/tools/qnitetool.h
+++ modules/Qnite/plugin/tools/qnitetool.h
@@ -9,6 +9,7 @@ class QniteTool: public QQuickItem
 {
   Q_OBJECT
   Q_PROPERTY(QniteAxes* axes READ axes NOTIFY axesChanged) // TODO: axes needs to be a property?
+
 public:
   explicit QniteTool(QQuickItem* parent = 0);
   virtual ~QniteTool() {}
--- tests/plugin/tst_qnitetool/tst_qnitetool.cpp
+++ tests/plugin/tst_qnitetool/tst_qnitetool.cpp
@@ -1,14 +1,27 @@
 #include <QtTest/QtTest>
 
 #include "tools/qnitetool.h"
+#include "qniteartist.h"
+#include "qniteaxes.h"
+
+class FooArtist : public QniteArtist
+{
+  Q_OBJECT
+
+public Q_SLOTS:
+  void processData() {}
+
+public:
+  FooArtist(QQuickItem * p=0) : QniteArtist(p) {}
+};
 
 class FooTool: public QniteTool
 {
   Q_OBJECT
+
 public:
-  FooTool(QQuickItem* parent = 0):
-    QniteTool(parent)
-  {}
+  FooTool(QQuickItem* parent = 0): QniteTool(parent) {}
+  QList<QniteArtist*> getFooArtists() { return artists(); }
 };
 
 class TestQniteTool: public QObject
@@ -17,14 +30,25 @@ class TestQniteTool: public QObject
 
 private slots:
 
-  void testInstance();
+  void testInstance()
+  {
+    FooTool f;
+    QniteAxes axes;
+    FooArtist a1, a2;
 
+    auto p1 = axes.artists();
+    p1.append(&p1, &a1);
+    p1.append(&p1, &a2);
+
+    auto p2 = axes.tools();
+    p2.append(&p2, &f);
+
+    QList<QniteArtist*> l{&a1, &a2};
+    QCOMPARE(f.getFooArtists(), l);
+  }
 };
 
-void TestQniteTool::testInstance()
-{
-  FooTool f;
-}
+
 
 QTEST_MAIN(TestQniteTool)
 #include "tst_qnitetool.moc"
