ef9c9668a23a834bc1c711ea68257ebc4169dc7b
Silvano Cerza <silvanocerza@gmail.com>
2019-04-02 16:34:48 +0200

    Added maximum zoom factor and zoom reset

--- src/tools/qnitezoomtool.cpp
+++ src/tools/qnitezoomtool.cpp
@@ -5,11 +5,17 @@
 #include "qnitezoompainter.h"
 
 QniteZoomTool::QniteZoomTool(QQuickItem *parent)
-    : QniteTool{parent}, m_zoomLevel{0}, m_pen{new QnitePen} {
+    : QniteTool{parent}, m_minZoomFactor{10e4},
+      m_limitZoom{true}, m_pen{new QnitePen} {
   setAcceptedMouseButtons(Qt::RightButton);
+
+  // Default pen style
   m_pen->setFill("#6EDCDCDC");
   m_pen->setStroke("#7E7E7E");
   m_pen->setWidth(1.0);
+
+  connect(this, &QniteZoomTool::axesChanged, this,
+          &QniteZoomTool::connectAxesBoundsSignals);
 }
 
 QniteZoomTool::~QniteZoomTool() {}
@@ -18,11 +24,44 @@ QNanoQuickItemPainter *QniteZoomTool::createItemPainter() const {
   return new QniteZoomPainter;
 }
 
+void QniteZoomTool::resetZoom() {
+  auto xAxis = axes()->axisX();
+  auto yAxis = axes()->axisY();
+
+  xAxis->setLowerBound(m_baseZoomRect.x());
+  xAxis->setUpperBound(m_baseZoomRect.width());
+  yAxis->setLowerBound(m_baseZoomRect.y());
+  yAxis->setUpperBound(m_baseZoomRect.height());
+  setEnabled(true);
+}
+
+void QniteZoomTool::setMinZoomFactor(qreal factor) {
+  if (qFuzzyCompare(1 + m_minZoomFactor, 1 + factor)) {
+    m_minZoomFactor = factor;
+    emit minZoomFactorChanged();
+  }
+}
+
+void QniteZoomTool::setLimitZoom(bool limit) {
+  if (m_limitZoom != limit) {
+    m_limitZoom = limit;
+    emit limitZoomChanged();
+  }
+}
+
 void QniteZoomTool::mousePressEvent(QMouseEvent *event) {
+  if (!isEnabled()) {
+    return;
+  }
+
   m_zoomRect.setTopLeft(event->pos());
 }
 
 void QniteZoomTool::mouseMoveEvent(QMouseEvent *event) {
+  if (!isEnabled()) {
+    return;
+  }
+
   m_zoomRect.setBottomRight(event->pos());
   update();
 }
@@ -61,4 +100,47 @@ void QniteZoomTool::mouseReleaseEvent(QMouseEvent *event) {
   m_zoomRect = QRectF{};
 
   update();
+
+  if (!m_limitZoom) {
+    return;
+  }
+
+  // Disables zoom if a certain amount of zoom is reached
+  auto minSize = minimumZoomSize();
+  auto axisYSize = qAbs(yMapped.first() - yMapped.last());
+  auto axisXSize = qAbs(xMapped.first() - xMapped.last());
+
+  if (axisXSize <= minSize.width() || axisYSize <= minSize.height()) {
+    setEnabled(false);
+  }
+}
+
+void QniteZoomTool::connectAxesBoundsSignals() const {
+  connect(axes(), &QniteAxes::xBoundsChanged, this,
+          &QniteZoomTool::updateBaseZoomRectXBounds);
+  connect(axes(), &QniteAxes::yBoundsChanged, this,
+          &QniteZoomTool::updateBaseZoomRectYBounds);
+  disconnect(this, &QniteZoomTool::axesChanged, this,
+             &QniteZoomTool::connectAxesBoundsSignals);
+}
+
+void QniteZoomTool::updateBaseZoomRectXBounds() {
+  auto xBounds = axes()->xBounds();
+  m_baseZoomRect.setX(xBounds.first());
+  m_baseZoomRect.setWidth(xBounds.last());
+  disconnect(axes(), &QniteAxes::xBoundsChanged, this,
+             &QniteZoomTool::updateBaseZoomRectXBounds);
+}
+
+void QniteZoomTool::updateBaseZoomRectYBounds() {
+  auto yBounds = axes()->yBounds();
+  m_baseZoomRect.setY(yBounds.first());
+  m_baseZoomRect.setHeight(yBounds.last());
+  disconnect(axes(), &QniteAxes::yBoundsChanged, this,
+             &QniteZoomTool::updateBaseZoomRectYBounds);
+}
+
+QSizeF QniteZoomTool::minimumZoomSize() const {
+  return {qAbs(m_baseZoomRect.width() / m_minZoomFactor),
+          qAbs(m_baseZoomRect.height() / m_minZoomFactor)};
 }
--- src/tools/qnitezoomtool.h
+++ src/tools/qnitezoomtool.h
@@ -6,6 +6,10 @@
 class QniteZoomTool : public QniteTool {
   Q_OBJECT
   Q_PROPERTY(QnitePen *pen READ pen CONSTANT)
+  Q_PROPERTY(qreal minZoomFactor READ minZoomFactor WRITE setMinZoomFactor
+                 NOTIFY minZoomFactorChanged)
+  Q_PROPERTY(
+      bool limitZoom READ limitZoom WRITE setLimitZoom NOTIFY limitZoomChanged)
 
 public:
   explicit QniteZoomTool(QQuickItem *parent = nullptr);
@@ -16,14 +20,34 @@ public:
   QRectF rect() const { return m_zoomRect; }
   QnitePen *pen() const { return m_pen.data(); }
 
+  qreal minZoomFactor() const { return m_minZoomFactor; }
+  void setMinZoomFactor(qreal factor);
+
+  bool limitZoom() const { return m_limitZoom; }
+  void setLimitZoom(bool limit);
+
+  Q_INVOKABLE void resetZoom();
+
+signals:
+  void minZoomFactorChanged() const;
+  void limitZoomChanged() const;
+
 protected:
   virtual void mousePressEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
   virtual void mouseMoveEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
   virtual void mouseReleaseEvent(QMouseEvent *event) Q_DECL_OVERRIDE;
 
-private:
-  int m_zoomLevel;
+private slots:
+  void connectAxesBoundsSignals() const;
+  void updateBaseZoomRectXBounds();
+  void updateBaseZoomRectYBounds();
 
+private:
   QRectF m_zoomRect;
+  QRectF m_baseZoomRect;
+  qreal m_minZoomFactor;
+  bool m_limitZoom;
   QScopedPointer<QnitePen> m_pen;
+
+  QSizeF minimumZoomSize() const;
 };
